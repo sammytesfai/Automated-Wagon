b20ba5d8a9b535d32f557fcd87d509dae6c6e2c6
Sammy Tesfai <sammytesfai@ymail.com>
2023-04-21 12:04:48 -0700

    Reformatted helper functions and placed them in misc module, all defines are now in misc.h

--- Main-Motors/Libraries/misc.h
+++ Main-Motors/Libraries/misc.h
@@ -1,11 +1,46 @@
 #ifndef _MISC_H__
 #define _MISC_H__
 
+// Used by PWM & Motors modules
 #define MAX_FREQ 100
 #define MAX_DUTY_CYCLE 255
 #define ACTIVE 1
 #define INACTIVE 0
 #define SUCCESS 1
 #define ERROR 0
+
+#define RIGHTPWM 5       // Set Speed of motor
+#define RIGHTFORWARD 31  // Move Forward
+#define RIGHTBACKWARD 32 // Move Backward
+#define RIGHTMOTORENCODERA 7
+#define RIGHTMOTORENCODERB 8
+
+#define LEFTPWM 3      // Set Speed of motor
+#define LEFTFORWARD 27  // Move Forward
+#define LEFTBACKWARD 26 // Move Backward
+#define LEFTMOTORENCODERB 35
+#define LEFTMOTORENCODERA 38
+
+// IR Camera Select Pins
+#define CAMERA_SEL_A0 11
+#define CAMERA_SEL_A1 12
+#define CAMERA_SEL_A2 13
+#define CAMERA_SEL_A3 39
+
+// I/O pins for US sensors
+#define LEFTTRIGGER 34
+#define RIGHTTRIGGER 37
+#define LEFTECHO 40
+#define RIGHTECHO 41
+
+// Values to define distane from US sensors and confidence samples
+#define CONFIDENCE_SAMPLE 5
+#define DISTANCE 8
+
+void set_pins(int flag);
+void printResult(int *positionX, int num);
+bool check_sides(int trig, int echo, char side);
+void echo_confidence(int trig, int echo, char side);
+int echo_avg(char side);
 
 #endif
--- Main-Motors/Main-Motors.ino
+++ Main-Motors/Main-Motors.ino
@@ -1,32 +1,6 @@
-                                                                                                                                                                                                                                                                                                              #define RIGHTPWM 5       // Set Speed of motor
-#define RIGHTPWM 5       // Set Speed of motor
-#define RIGHTFORWARD 31  // Move Forward
-#define RIGHTBACKWARD 32 // Move Backward
-#define RIGHTMOTORENCODERA 7
-#define RIGHTMOTORENCODERB 8
-
-#define LEFTPWM 3      // Set Speed of motor
-#define LEFTFORWARD 27  // Move Forward
-#define LEFTBACKWARD 26 // Move Backward
-#define LEFTMOTORENCODERB 35
-#define LEFTMOTORENCODERA 38
-#define LEFTBUMPER 33
-
-#define CAMERA_SEL_A0 11
-#define CAMERA_SEL_A1 12
-#define CAMERA_SEL_A2 13
-#define CAMERA_SEL_A3 39
-
-#define LEFTTRIGGER 34
-#define RIGHTTRIGGER 37
-#define LEFTECHO 40
-#define RIGHTECHO 41
-
 #include "Libraries\motors.h"
 #include "Libraries\DFRobotIRPosition.h"
-
-int left_echo [5];
-int right_echo [5];
+#include "Libraries\misc.h"
 
 MOTORS M(RIGHTPWM, LEFTPWM, RIGHTFORWARD, RIGHTBACKWARD, LEFTFORWARD, LEFTBACKWARD);
 DFRobotIRPosition myDFRobotIRPosition;
@@ -49,7 +23,6 @@ void setup()
   // Left Motor
   pinMode(LEFTMOTORENCODERB, INPUT);
   pinMode(LEFTMOTORENCODERA, INPUT);
-  pinMode(LEFTBUMPER, INPUT);
 
   pinMode(CAMERA_SEL_A0, OUTPUT);
   pinMode(CAMERA_SEL_A1, OUTPUT);
@@ -83,18 +56,16 @@ void setup()
 //      positionYL[i]=myDFRobotIRPosition.readY(i);
 //    }
 //
-//    printResult(2);
+//    printResult(positionXL, 2);
 //  }
 //}
 
-long duration;
-int distance;
 void loop()
 {
   while(check_sides(LEFTTRIGGER, LEFTECHO, 'L'))
   {
     echo_confidence(LEFTTRIGGER, LEFTECHO, 'L');
-    if(echo_avg('L') < 8)
+    if(echo_avg('L') < DISTANCE)
     {
       M.Forward_Left(255);
       Serial.println("LEFT");
@@ -108,7 +79,7 @@ void loop()
   while(check_sides(RIGHTTRIGGER, RIGHTECHO, 'R'))
   {
     echo_confidence(RIGHTTRIGGER, RIGHTECHO, 'R');
-    if(echo_avg('R') < 8)
+    if(echo_avg('R') < DISTANCE)
     {
       M.Forward_Right(255);
       Serial.println("RIGHT");
@@ -132,7 +103,7 @@ void loop()
       positionYR[i]=myDFRobotIRPosition.readY(i);
     }
 
-    printResult(1);
+    printResult(positionXR, 1);
   }
   else
   {
@@ -154,7 +125,7 @@ void loop()
       positionYL[i]=myDFRobotIRPosition.readY(i);
     }
 
-    printResult(2);
+    printResult(positionXL, 2);
   }
   else
   {
@@ -219,115 +190,6 @@ void loop()
 //  delay(1000);
 //  i = (i+1)%7;
 //}
-
-void set_pins(int flag)
-{
-  if(flag)
-  {
-    digitalWrite(CAMERA_SEL_A0, HIGH);
-    digitalWrite(CAMERA_SEL_A1, HIGH);
-    digitalWrite(CAMERA_SEL_A2, LOW);
-    digitalWrite(CAMERA_SEL_A3, LOW);
-  }
-  else
-  {
-    digitalWrite(CAMERA_SEL_A0, LOW);
-    digitalWrite(CAMERA_SEL_A1, LOW);
-    digitalWrite(CAMERA_SEL_A2, HIGH);
-    digitalWrite(CAMERA_SEL_A3, HIGH);
-  }
-}
-
-void printResult(int num)
-{
-  if(num == 1)
-  {
-    Serial.print("Camera1: ");
-    for (int i=0; i<4; i++) {
-      Serial.print(positionXR[i]);
-      Serial.print(",");
-      
-      Serial.print(positionYR[i]);
-      Serial.print(";");
-    }
-  }
-  else
-  {
-    Serial.print("Camera2: ");
-    for (int i=0; i<4; i++) {
-      Serial.print(positionXL[i]);
-      Serial.print(",");
-      
-      Serial.print(positionYL[i]);
-      Serial.print(";");
-    }
-  }
-  Serial.println();
-}
-
-void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
-  analogWrite(pwm,pwmVal);
-  if(dir == 1){
-    digitalWrite(in1,HIGH);
-    digitalWrite(in2,LOW);
-  }
-  else if(dir == -1){
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,HIGH);
-  }
-  else{
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,LOW);
-  }  
-}
-
-
-bool check_sides(int trig, int echo, char side)
-{
-  digitalWrite(trig, LOW);
-  delayMicroseconds(2);
-
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-
-  duration = pulseIn(echo, HIGH);
-  distance = duration*0.034/2;
-  if(distance < 8)
-  {
-    if(side == 'L') left_echo[0] = distance;
-    else right_echo[0] = distance;
-    return true;
-  }
-  return false;
-}
-
-void echo_confidence(int trig, int echo, char side)
-{
-  for(int i  = 0; i < 4; i++)
-  {
-    digitalWrite(trig, LOW);
-    delayMicroseconds(2);
-
-    digitalWrite(trig, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trig, LOW);
-
-    duration = pulseIn(echo, HIGH);
-    distance = duration*0.034/2;
-
-    if(side == 'L') left_echo[i+1] = distance;
-    else right_echo[i+1] = distance;
-  }
-}
-
-int echo_avg(char side)
-{
-  int total = 0;
-  if(side == 'L')for(int i = 0; i < 5; i++) total += left_echo[i];
-  else for(int i = 0; i < 5; i++) total += right_echo[i];
-  return total/5;
-}
 
 
 
