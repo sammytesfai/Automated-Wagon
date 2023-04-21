daf79e85001e6ba73ab210058d9e181dfd0be25e
Sammy Tesfai <sammytesfai@ymail.com>
2023-04-20 17:01:59 -0700

    Basic collision avoidance using two US on both sides of the wagon

--- Main-Motors/Main-Motors.ino
+++ Main-Motors/Main-Motors.ino
@@ -12,41 +12,30 @@
 #define LEFTMOTORENCODERA 38
 #define LEFTBUMPER 33
 
-#define CAMERAMUXA 11
-#define CAMERAMUXB 12
-#define CAMERAMUXC 40
-#define CAMERAMUXD 41
+#define CAMERA_SEL_A0 11
+#define CAMERA_SEL_A1 12
+#define CAMERA_SEL_A2 13
+#define CAMERA_SEL_A3 39
 
-#define LEFTBACKTRIG 39
-#define LEFTBACKECHO 13
-
-#define NMOTORS 2
+#define LEFTTRIGGER 34
+#define RIGHTTRIGGER 37
+#define LEFTECHO 40
+#define RIGHTECHO 41
 
 #include "Libraries\motors.h"
 #include "Libraries\DFRobotIRPosition.h"
 
+int left_echo [5];
+int right_echo [5];
+
 MOTORS M(RIGHTPWM, LEFTPWM, RIGHTFORWARD, RIGHTBACKWARD, LEFTFORWARD, LEFTBACKWARD);
 DFRobotIRPosition myDFRobotIRPosition;
 
-// Pins
-const int enca[] = {RIGHTMOTORENCODERA,LEFTMOTORENCODERA};
-const int encb[] = {RIGHTMOTORENCODERB,LEFTMOTORENCODERB};
-const int pwm[] = {RIGHTPWM,LEFTPWM};
-const int in1[] = {RIGHTFORWARD,LEFTFORWARD};
-const int in2[] = {RIGHTBACKWARD,LEFTBACKWARD};
+int positionXR[4];     ///< Store the X position
+int positionYR[4];     ///< Store the Y position
 
-// Globals
-long prevT = 0;
-volatile int posi[] = {0,0};
-
-int positionX[4];     ///< Store the X position
-int positionY[4];     ///< Store the Y position
-
-int positionX1[4];     ///< Store the X position
-int positionY1[4];     ///< Store the Y position
-
-int positionX2[4];     ///< Store the X position
-int positionY2[4];     ///< Store the Y position
+int positionXL[4];     ///< Store the X position
+int positionYL[4];     ///< Store the Y position
 
 int randomarray[] = {300, 600, 500, 900, 1023, 700, 100};
 
@@ -62,47 +51,85 @@ void setup()
   pinMode(LEFTMOTORENCODERA, INPUT);
   pinMode(LEFTBUMPER, INPUT);
 
-  pinMode(CAMERAMUXA, OUTPUT);
-  pinMode(CAMERAMUXB, OUTPUT);
-  pinMode(CAMERAMUXC, OUTPUT);
-  pinMode(CAMERAMUXD, OUTPUT);
-  
+  pinMode(CAMERA_SEL_A0, OUTPUT);
+  pinMode(CAMERA_SEL_A1, OUTPUT);
+  pinMode(CAMERA_SEL_A2, OUTPUT);
+  pinMode(CAMERA_SEL_A3, OUTPUT);
+
+  // Right Camera
   set_pins(0);
   myDFRobotIRPosition.begin();
 
-  
+  // Left Camera
   set_pins(1);
   myDFRobotIRPosition.begin();
 
-  pinMode(LEFTBACKTRIG, OUTPUT);
-  pinMode(LEFTBACKECHO, INPUT);
+  pinMode(LEFTTRIGGER, OUTPUT);
+  pinMode(RIGHTTRIGGER, OUTPUT);
+  pinMode(LEFTECHO, INPUT);
+  pinMode(RIGHTECHO, INPUT);
 }
+
+//void loop()
+//{
+//  delay(50);
+//  myDFRobotIRPosition.requestPosition();
+//
+//  if (myDFRobotIRPosition.available()) 
+//  {
+//    for (int i=0; i<4; i++) 
+//    {
+//      positionXL[i]=myDFRobotIRPosition.readX(i);
+//      positionYL[i]=myDFRobotIRPosition.readY(i);
+//    }
+//
+//    printResult(2);
+//  }
+//}
 
 long duration;
 int distance;
 void loop()
 {
-  while(check_sides(LEFTBACKTRIG, LEFTBACKECHO))
+  while(check_sides(LEFTTRIGGER, LEFTECHO, 'L'))
   {
-    M.Forward_Right(255);
-    Serial.println("RIGHT");
-    delay(250);
-    M.Backward(255);
-    Serial.println("Forward");
-    delay(250);
-    
+    echo_confidence(LEFTTRIGGER, LEFTECHO, 'L');
+    if(echo_avg('L') < 8)
+    {
+      M.Forward_Left(255);
+      Serial.println("LEFT");
+      delay(100);
+      M.Backward(255);
+      Serial.println("Forward");
+      delay(100);
+    }
   }
-  
+
+  while(check_sides(RIGHTTRIGGER, RIGHTECHO, 'R'))
+  {
+    echo_confidence(RIGHTTRIGGER, RIGHTECHO, 'R');
+    if(echo_avg('R') < 8)
+    {
+      M.Forward_Right(255);
+      Serial.println("RIGHT");
+      delay(100);
+      M.Backward(255);
+      Serial.println("Forward");
+      delay(100);
+    }
+  }
+
+  // Right Camera
   set_pins(0);
   delay(50);
   
   myDFRobotIRPosition.requestPosition();
-  if (myDFRobotIRPosition.available()) 
+  if (myDFRobotIRPosition.available())
   {
-    for (int i=0; i<4; i++) 
+    for (int i=0; i<4; i++)
     {
-      positionX1[i]=myDFRobotIRPosition.readX(i);
-      positionY1[i]=myDFRobotIRPosition.readY(i);
+      positionXR[i]=myDFRobotIRPosition.readX(i);
+      positionYR[i]=myDFRobotIRPosition.readY(i);
     }
 
     printResult(1);
@@ -112,6 +139,8 @@ void loop()
     Serial.println("Device not available!");
   }
 
+
+  // Left Camera
   set_pins(1);
   delay(50);
   
@@ -121,8 +150,8 @@ void loop()
   {
     for (int i=0; i<4; i++) 
     {
-      positionX2[i]=myDFRobotIRPosition.readX(i);
-      positionY2[i]=myDFRobotIRPosition.readY(i);
+      positionXL[i]=myDFRobotIRPosition.readX(i);
+      positionYL[i]=myDFRobotIRPosition.readY(i);
     }
 
     printResult(2);
@@ -132,15 +161,15 @@ void loop()
     Serial.println("Device not available!");
   }
 
-  if(positionX1[0] > 300 && positionX1[0] < 1000 && positionX2[0] > 300 && positionX2[0] < 1000)
+  if(positionXR[0] > 300 && positionXR[0] < 1000 && positionXL[0] > 300 && positionXL[0] < 1000)
   {
     M.Backward(255);
   }
-  else if(positionX1[0] < 300)
+  else if(positionXR[0] < 300)
   {
     M.Forward_Right(255);
   }
-  else if(positionX2[0] < 300)
+  else if(positionXL[0] < 300)
   {
     M.Forward_Left(255);
   }
@@ -149,45 +178,6 @@ void loop()
     M.STOP();
   }
 }
-
-
-//void loop() {
-//
-//  // set target position
-//  int target[NMOTORS];
-//  target[0] = 750*sin(prevT/1e6);
-//  target[1] = -750*sin(prevT/1e6);
-//
-//  // time difference
-//  long currT = micros();
-//  float deltaT = ((float) (currT - prevT))/( 1.0e6 );
-//  prevT = currT;
-//
-//  // Read the position
-//  int pos[NMOTORS];
-//  noInterrupts(); // disable interrupts temporarily while reading
-//  for(int k = 0; k < NMOTORS; k++){
-//      pos[k] = posi[k];
-//    }
-//  interrupts(); // turn interrupts back on
-//  
-//  // loop through the motors
-//  for(int k = 0; k < NMOTORS; k++){
-//    int pwr, dir;
-//    // evaluate the control signal
-//    pid[k].evalu(pos[k],target[k],deltaT,pwr,dir);
-//    // signal the motor
-//    setMotor(dir,pwr,pwm[k],in1[k],in2[k]);
-//  }
-//
-//  for(int k = 0; k < NMOTORS; k++){
-//    Serial.print(target[k]);
-//    Serial.print(" ");
-//    Serial.print(pos[k]);
-//    Serial.print(" ");
-//  }
-//  Serial.println();
-//}
 
 // Check bumpers
 //void loop() 
@@ -234,17 +224,17 @@ void set_pins(int flag)
 {
   if(flag)
   {
-    digitalWrite(CAMERAMUXA, HIGH);
-    digitalWrite(CAMERAMUXB, HIGH);
-    digitalWrite(CAMERAMUXC, LOW);
-    digitalWrite(CAMERAMUXD, LOW);
+    digitalWrite(CAMERA_SEL_A0, HIGH);
+    digitalWrite(CAMERA_SEL_A1, HIGH);
+    digitalWrite(CAMERA_SEL_A2, LOW);
+    digitalWrite(CAMERA_SEL_A3, LOW);
   }
   else
   {
-    digitalWrite(CAMERAMUXA, LOW);
-    digitalWrite(CAMERAMUXB, LOW);
-    digitalWrite(CAMERAMUXC, HIGH);
-    digitalWrite(CAMERAMUXD, HIGH);
+    digitalWrite(CAMERA_SEL_A0, LOW);
+    digitalWrite(CAMERA_SEL_A1, LOW);
+    digitalWrite(CAMERA_SEL_A2, HIGH);
+    digitalWrite(CAMERA_SEL_A3, HIGH);
   }
 }
 
@@ -254,10 +244,10 @@ void printResult(int num)
   {
     Serial.print("Camera1: ");
     for (int i=0; i<4; i++) {
-      Serial.print(positionX1[i]);
+      Serial.print(positionXR[i]);
       Serial.print(",");
       
-      Serial.print(positionY1[i]);
+      Serial.print(positionYR[i]);
       Serial.print(";");
     }
   }
@@ -265,10 +255,10 @@ void printResult(int num)
   {
     Serial.print("Camera2: ");
     for (int i=0; i<4; i++) {
-      Serial.print(positionX2[i]);
+      Serial.print(positionXL[i]);
       Serial.print(",");
       
-      Serial.print(positionY2[i]);
+      Serial.print(positionYL[i]);
       Serial.print(";");
     }
   }
@@ -291,28 +281,8 @@ void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
   }  
 }
 
-void readEncoderRight(){
-  int b = digitalRead(encb[0]);
-  if(b > 0){
-    posi[0]++;
-  }
-  else{
-    posi[0]--;
-  }
-}
 
-
-void readEncoderLeft(){
-  int b = digitalRead(encb[1]);
-  if(b > 0){
-    posi[1]++;
-  }
-  else{
-    posi[1]--;
-  }
-}
-
-bool check_sides(int trig, int echo)
+bool check_sides(int trig, int echo, char side)
 {
   digitalWrite(trig, LOW);
   delayMicroseconds(2);
@@ -323,8 +293,40 @@ bool check_sides(int trig, int echo)
 
   duration = pulseIn(echo, HIGH);
   distance = duration*0.034/2;
-  if(distance < 4)return true;
+  if(distance < 8)
+  {
+    if(side == 'L') left_echo[0] = distance;
+    else right_echo[0] = distance;
+    return true;
+  }
   return false;
+}
+
+void echo_confidence(int trig, int echo, char side)
+{
+  for(int i  = 0; i < 4; i++)
+  {
+    digitalWrite(trig, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig, LOW);
+
+    duration = pulseIn(echo, HIGH);
+    distance = duration*0.034/2;
+
+    if(side == 'L') left_echo[i+1] = distance;
+    else right_echo[i+1] = distance;
+  }
+}
+
+int echo_avg(char side)
+{
+  int total = 0;
+  if(side == 'L')for(int i = 0; i < 5; i++) total += left_echo[i];
+  else for(int i = 0; i < 5; i++) total += right_echo[i];
+  return total/5;
 }
 
 
